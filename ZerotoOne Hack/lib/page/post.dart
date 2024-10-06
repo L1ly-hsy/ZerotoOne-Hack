41537110139f94c8443f4e42f64cb8031c0cd523
u7787195 <u7787195@anu.edu.au>
2024-10-06 13:38:34 +1100

    update

---
+++ ZerotoOne Hack/lib/page/post.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Post extends StatefulWidget{
+  const Post ({super.key});
+  @override
+  State<Post>createState() => _PostState();
+}
+class _Post extends State<Post>{
+
+}
